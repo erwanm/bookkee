c36c575b723a2eef1bcafe30e9cacda53c848f63
Erwan <erwan.more@gmail.com>
2025-01-20 22:10:17 +0100

    stuff

--- app.py
+++ app.py
@@ -1,34 +1,31 @@
 import streamlit as st
 import pandas as pd
 
-from bookkee.accounts import sheet_names, empty_data, columns
-from bookkee.accounts import read_books
+from bookkee.accounts import Account
 
 
 def init_session_state():
     if 'data' not in st.session_state:
-        st.session_state['data'] = empty_data
+        st.session_state['accounts'] = {}
 
 @st.dialog("New account")
-def new_account(data):
+def new_account():
+    # TODO error if account name already exists
     account_name = st.text_input("Account name")
     filename = st.text_input("Save as ...", value = account_name+'.xlsx' )
     if not filename.endswith('.xlsx'):
          filename = filename + '.xlsx'
     if st.button("Submit"):
-        for sheet in sheet_names:
-            data[sheet][account_name] = columns[sheet]
-            data['filename'][account_name] = filename
+        st.session_state.accounts[account_name] = Account(file = filename)
 
 
 
 def entry_point():
     if st.button('Create new account'):
-        new_account(st.session_state['data'])
-    st.write('DEBUG ', st.session_state['data']['book'])
+        new_account()
     uploaded_file = st.file_uploader("Choose an existing accounting book")
     if uploaded_file is not None:
-        full_books = read_books(uploaded_file)
+        st.session_state.accounts = Account.read_books(file = uploaded_file)
         # st.write(dataframe)
         bank_statement_file = st.file_uploader("Read a new bank statement")
         if bank_statement_file is not None:
--- bookkee/accounts.py
+++ bookkee/accounts.py
@@ -1,8 +1,9 @@
 
+from abc import ABC
+import pandas as pd
 
 sheet_names = [ 'book', 'rules', 'conf' ]
 
-empty_data = { sheet: {}  for sheet in sheet_names }
 # TODO
 columns = { 
     'book': [],
@@ -10,24 +11,39 @@ columns = {
     'conf': []
 }
 
-def read_books(uploaded_file):
-    filename = uploaded_file if isinstance(uploaded_file, str) else uploaded_file.name
-    try:
-        all_excel_sheets = pd.read_excel(uploaded_file, None)
-    except:
-        raise Exception(f'The file {filename} is not a valid Excel file.')
-    valid_sheet_names = [ sheet_name for sheet_name in all_excel_sheets.keys() if sheet_name.endswith('.book') or sheet_name.endswith('.rules') or sheet_name.endswith('.conf') ]
-    accounts_names = set([ sheet.split('.')[:-1] for sheet in valid_sheet_names ])
-    data = {}
-    for sheet in sheet_names:
-        data[sheet] = {}
+class Account(ABC):
+
+    def __init__(self, file, book = None, rules = None, conf = None):
+        self.file = file
+        self.book = pd.DataFrame(columns=columns['book']) if book is None else book
+        # TODO: format rules?
+        self.rules = pd.DataFrame(columns=columns['rules']) if rules is None else rules
+        # TODO: default conf?
+        self.conf = {} if conf is None else None
+        return self
+
+    @staticmethod
+    def read_books(uploaded_file):
+        filename = uploaded_file if isinstance(uploaded_file, str) else uploaded_file.name
+        try:
+            all_excel_sheets = pd.read_excel(uploaded_file, None)
+        except:
+            raise Exception(f'The file {filename} is not a valid Excel file.')
+        valid_sheet_names = [ sheet_name for sheet_name in all_excel_sheets.keys() if sheet_name.endswith('.book') or sheet_name.endswith('.rules') or sheet_name.endswith('.conf') ]
+        accounts_names = set([ sheet.split('.')[:-1] for sheet in valid_sheet_names ])
+        accounts = {}
         for account in accounts_names:
-            if f'{account}.{sheet}' in  valid_sheet_names:
-                data[sheet][account] = all_excel_sheets[ f'{account}.{sheet}' ]
-            else:
-                raise Exception(f'{account}.{sheet} not found among sheet names in {filename}')   
-    for account in accounts_names:
-        data['filename'] = filename
-    # conf = parse_general_config(all_excel_sheets['general.conf'])
-    return data
+            data = {}
+            for sheet in sheet_names:
+                if f'{account}.{sheet}' in  valid_sheet_names:
+                    data[sheet] = all_excel_sheets[ f'{account}.{sheet}' ]
+                else:
+                    raise Exception(f'{account}.{sheet} not found among sheet names in {filename}')   
+            accounts[account] = Account(file = filename,
+                                        book = data['book'],
+                                        rules = data['rules'],
+                                        conf = data['conf']
+                                        )
+            # conf = parse_general_config(all_excel_sheets['general.conf'])
+        return accounts
 
